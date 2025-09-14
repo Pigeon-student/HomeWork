247bd6cbcd2d8a02f3f45688f5b478eebf5a7ab8
AndrewPigeon <andrewpigeon904@gmail.com>
2025-09-14 15:37:10 +0300

    Pushing homework part 2 by 14.09.2025

---
+++ Arrays/Game.cs
@@ -0,0 +1,552 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Design;
+using System.Drawing;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Runtime.ConstrainedExecution;
+using System.Runtime.InteropServices;
+using System.Runtime.InteropServices.Marshalling;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZeroCrossGame
+{
+    class Game
+    {
+        static void Main()
+        {
+            Console.InputEncoding = System.Text.Encoding.UTF8;
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+            
+            Console.WriteLine("Hello, let's play a game!");            
+
+            Console.Write("\nWhat is 1st player name? Tell me: ");
+            string firstPlayerName = Console.ReadLine();
+            Console.Write("What is 2nd player name? Sign it: ");
+            string secondPlayerName = Console.ReadLine();
+
+            
+            Console.WriteLine("\n\tLet's start!\n\n");
+
+            // VARIABLES
+            string zero = "O";
+            string cross = "X";
+
+            int userChoise = 0;
+
+
+            const int RAWS = 3;
+            const int COLLS = 3;
+
+            int counter = 0;
+            const int TRY = 9;
+
+            bool isPlayerOne = true;            
+
+            // START
+
+            string[,] field = new string[RAWS, COLLS]
+                {
+                { "1", "2", "3" },
+                { "4", "5", "6" },
+                { "7", "8", "9" }
+                };
+
+            DrawGrid(field);
+
+            for (int i = 0; i < RAWS; i++)
+            {              
+                for (int j = 0; j < COLLS; j++)
+                {
+                    Console.Write($"{field[i, j]}");
+                    if (j < COLLS - 1) Console.Write(" | ");
+                }
+                Console.WriteLine();
+                
+                if (i < RAWS - 1)
+                {
+                    Console.WriteLine("----------");
+                }
+            }
+
+            // INSANE START
+
+            while (counter < TRY)
+            {
+
+
+
+                if (isPlayerOne)
+                {
+
+                    Console.Write($"\nLet's {firstPlayerName} choose number to put {cross} in it: ");
+                    userChoise = Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine();
+
+                    switch (userChoise)
+                    {
+                        case 1:
+
+                            if (field[0, 0] != cross && field[0, 0] != zero)
+                            {
+                                field[0, 0] = cross;                               
+                            }
+                            else
+                            {
+                                Console.WriteLine("This index is full. Choose another one.");
+                                counter--;
+                            }
+                            break;
+
+                        case 2:
+
+                            if (field[0, 1] != cross && field[0, 1] != zero)
+                            {
+                                field[0, 1] = cross;                               
+                            }
+                            else
+                            {
+                                Console.WriteLine("This index is full. Choose another one.");
+                                counter--;
+                            }
+                            break;
+
+                        case 3:
+
+                            if (field[0, 2] != cross && field[0, 2] != zero)
+                            {
+                                field[0, 2] = cross;                                                        
+                            }
+
+                            else
+                            {
+                                Console.WriteLine("This index is full. Choose another one.");
+                                counter--;
+                            }
+
+                            break;
+
+                        case 4:
+
+                            if (field[1, 0] != cross && field[1, 0] != zero)
+                            {
+                                field[1, 0] = cross;                                                       
+                            }
+
+                            else
+                            {
+                                Console.WriteLine("This index is full. Choose another one.");
+                                counter--;
+                            }
+                            break;
+
+                        case 5:
+
+                            if (field[1, 1] != cross && field[1, 1] != zero)
+                            {
+                                field[1, 1] = cross;                                                          
+                            }
+
+                            else
+                            {
+                                Console.WriteLine("This index is full. Choose another one.");
+                                counter--;
+                            }
+                            break;
+
+                        case 6:
+
+                            if (field[1, 2] != cross && field[1, 2] != zero)
+                            {
+                                field[1, 2] = cross;                                                           
+                            }
+
+                            else
+                            {
+                                Console.WriteLine("This index is full. Choose another one.");
+                                counter--;
+                            }
+                            break;
+
+                        case 7:
+
+                            if (field[2, 0] != cross && field[2, 0] != zero)
+                            {
+                                field[2, 0] = cross;                                                     
+                            }
+
+                            else
+                            {
+                                Console.WriteLine("This index is full. Choose another one.");
+                                counter--;
+                            }
+                            break;
+
+                        case 8:
+
+                            if (field[2, 1] != cross && field[2, 1] != zero)
+                            {
+                                field[2, 1] = cross;                                                  
+                            }
+
+                            else
+                            {
+                                Console.WriteLine("This index is full. Choose another one.");
+                                counter--;
+                            }
+                            break;
+
+                        case 9:
+
+                            if (field[2, 2] != cross && field[2, 2] != zero)
+                            {
+                                field[2, 2] = cross;                                            
+                            }
+
+                            else
+                            {
+                                Console.WriteLine("This index is full. Choose another one.");
+                                counter--;
+                            }
+                            break;
+                    }
+
+                    isPlayerOne = false;
+                    counter++;
+                    Console.WriteLine();
+                }
+
+
+                else
+                {
+                    Console.Write($"\nLet's {secondPlayerName} choose number to put {zero} in it: ");
+                    userChoise = Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine();
+
+                    switch (userChoise)
+                    {
+                        case 1:
+
+                            if (field[0, 0] != cross && field[0, 0] != zero)
+                            {
+                                field[0, 0] = zero;                                                            
+                            }
+
+                            else
+                            {
+                                Console.WriteLine("This index is full. Choose another one.");
+                                counter--;
+                            }
+
+                            break;
+
+                        case 2:
+
+                            if (field[0, 1] != cross && field[0, 1] != zero)
+                            {
+                                field[0, 1] = zero;                                                      
+                            }
+
+                            else
+                            {
+                                Console.WriteLine("This index is full. Choose another one.");
+                                counter--;
+                            }
+                            break;
+
+                        case 3:
+
+                            if (field[0, 2] != cross && field[0, 2] != zero)
+                            {
+                                field[0, 2] = zero;                                             
+                            }
+
+                            else
+                            {
+                                Console.WriteLine("This index is full. Choose another one.");
+                                counter--;
+                            }
+
+                            break;
+
+                        case 4:
+
+                            if (field[1, 0] != cross && field[1, 0] != zero)
+                            {
+                                field[1, 0] = zero;                                                       
+                            }
+
+                            else
+                            {
+                                Console.WriteLine("This index is full. Choose another one.");
+                                counter--;
+                            }
+
+                            break;
+
+                        case 5:
+
+                            if (field[1, 1] != cross && field[1, 1] != zero)
+                            {
+                                field[1, 1] = zero;                                                       
+                            }
+
+                            else
+                            {
+                                Console.WriteLine("This index is full. Choose another one.");
+                                counter--;
+                            }
+                            break;
+
+                        case 6:
+
+                            if (field[1, 2] != cross && field[1, 2] != zero)
+                            {
+                                field[1, 2] = zero;                                                       
+                            }
+
+                            else
+                            {
+                                Console.WriteLine("This index is full. Choose another one.");
+                                counter--;
+                            }
+                            break;
+
+                        case 7:
+
+                            if (field[2, 0] != cross && field[2, 0] != zero)
+                            {
+                                field[2, 0] = zero;                                                      
+                            }
+
+                            else
+                            {
+                                Console.WriteLine("This index is full. Choose another one.");
+                                counter--;
+                            }
+                            break;
+
+                        case 8:
+
+                            if (field[2, 1] != cross && field[2, 1] != zero)
+                            {
+                                field[2, 1] = zero;                               
+                                
+                            }
+
+                            else
+                            {
+                                Console.WriteLine("This index is full. Choose another one.");
+                                counter--;
+                            }
+                            break;
+
+                        case 9:
+
+                            if (field[2, 2] != cross && field[2, 2] != zero)
+                            {
+                                field[2, 2] = zero;                                                        
+                            }
+
+                            else
+                            {
+                                Console.WriteLine("This index is full. Choose another one.");
+                                counter--;
+                            }
+                            break;
+
+                    }
+
+                    isPlayerOne = true;
+                    counter++;
+                    Console.WriteLine();
+                }
+
+                // Перевірка по рядках
+                for (int raw = 0; raw < RAWS; raw++)
+                {
+                    if (field[raw, 0] == cross &&
+                        field[raw, 1] == cross &&
+                        field[raw, 2] == cross)
+                    {
+                        Console.WriteLine($"\tGame over! {firstPlayerName} is win!-");
+                        goto End;
+                    }
+
+                    if (field[raw, 0] == zero &&
+                        field[raw, 1] == zero &&
+                        field[raw, 2] == zero)
+                    {
+                        Console.WriteLine($"\tGame over! {secondPlayerName} is win!-");
+                        goto End;                        
+                    }
+                }
+
+                // Перевірка по стовпчиках
+
+                for (int coll = 0; coll < COLLS; coll++)
+                {
+                    if (field[0, coll] == cross &&
+                        field[1, coll] == cross &&
+                        field[2, coll] == cross)
+                    {
+                        Console.WriteLine($"\tGame over! {firstPlayerName} is win!|");
+                        goto End;                       
+                    }
+
+                    if (field[0, coll] == zero &&
+                        field[1, coll] == zero &&
+                        field[2, coll] == zero)
+                    {
+                        Console.WriteLine($"\tGame over! {secondPlayerName} is win!|");
+                        goto End;                        
+                    }
+                }
+
+
+
+                // Перевірка по діагоналях
+
+                for (int diag = 0; diag < COLLS; diag++)
+                {
+                    if (field[0, 0] == cross &&
+                        field[1, 1] == cross &&
+                        field[2, 2] == cross)
+                    {
+                        Console.WriteLine($"\tGame over! {firstPlayerName} is win! > ");
+                        goto End;                       
+                    }
+
+                    if (field[0, 0] == zero &&
+                       field[1, 1] == zero &&
+                       field[2, 2] == zero)
+                    {
+                        Console.WriteLine($"\tGame over! {secondPlayerName} is win! > ");
+                        goto End;                       
+                    }
+                }
+
+                for (int diag = 0; diag < COLLS; diag++)
+                {
+                    if (field[2, diag] == cross &&
+                        field[1, diag] == cross &&
+                        field[0, diag] == cross)
+                    {
+                        Console.WriteLine($"\tGame over! {firstPlayerName} is win!/");
+                        goto End;                        
+                    }
+
+                    if (field[2, diag] == zero &&
+                        field[1, diag] == zero &&
+                        field[0, diag] == zero)
+                    {
+                        Console.WriteLine($"\tGame over! {secondPlayerName} is win! /");
+                        goto End;                        
+                    }
+                }
+
+                for (int i = 0; i < RAWS; i++)
+                {
+                    // рядок із символами
+                    for (int j = 0; j < COLLS; j++)
+                    {
+                        Console.Write($"{field[i, j]}");
+
+                        if (j < COLLS - 1)             // AI hint
+                        {
+                            Console.Write(" | ");
+                        }
+                    }
+                    Console.WriteLine();
+
+                    // розділювач між рядками           // AI hint
+                    if (i < RAWS - 1)
+                    {
+                        Console.WriteLine("----------");
+                    }
+                }
+            }
+
+        End:
+            if (counter == TRY)
+            {
+                Console.WriteLine("\n\tGame Over! It's a draw!");
+            }
+        }
+
+        static void DrawGrid(string[,] field)                          // AI hint
+        {
+
+        }
+    }
+}
+
+
+
+
+
+
+
+/*  BODY OF THE GAME
+
+     Console.Write($"\nLet's {firstPlayerName} choose number to put X in it: "); // {firstPlayerName} and X must be coloured
+    userOneChoise = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine();
+
+
+
+    switch (userOneChoise)
+    {
+        case 1:
+            field[0, 0] = cross;
+            break;
+
+        case 2:
+            field[0, 1] = cross;
+            break;
+
+        case 3:
+            field[0, 2] = cross;
+            break;
+
+        case 4:
+            field[1, 0] = cross;
+            break;
+
+        case 5:
+            field[1, 1] = cross;
+            break;
+
+        case 6:
+            field[1, 2] = cross;
+            break;
+
+        case 7:
+            field[2, 0] = cross;
+            break;
+
+        case 8:
+            field[2, 1] = cross;
+            break;
+
+        case 9:
+            field[2, 2] = cross;
+            break;
+
+        default:
+            Console.WriteLine("Wrong number");
+            break;
+    }
+
+    for (int i = 0; i < RAWS; i++) // Виввід значень з масиву
+    {
+        for (int j = 0; j < COLLS; j++)
+        {
+            Console.Write($"\t{field[i, j]}");
+        }
+
+        Console.WriteLine();
+    }
+
+ */
