dbb6ed51a1b7564ebef04fb5b90f1fb65a29785a
AndrewPigeon <andrewpigeon904@gmail.com>
2025-09-20 12:41:42 +0300

    HomeWork by 20.09.2025

---
+++ WhiteSpaces Replace/Program.cs
@@ -0,0 +1,24 @@
+﻿using System.Diagnostics.Metrics;
+
+class WhiteSpaces
+{
+    static void Main()
+    {
+        Console.InputEncoding = System.Text.Encoding.Unicode;
+        Console.OutputEncoding = System.Text.Encoding.Unicode;
+
+        Console.Write("Enter string: ");
+        System.Text.StringBuilder text = new System.Text.StringBuilder(Console.ReadLine());
+        
+        int counter =0;
+        for (int i = 0; i < text.Length; i++)
+        {
+            if (text[i] == ' ')
+            {
+                counter++;
+                text = text.Remove(i, 1);
+            }
+        }
+        Console.WriteLine($"We have deleted {counter} whitespaces.\n{text.ToString()}");        
+    }
+}
